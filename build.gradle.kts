3844ddde870eaf0c32781e86dca130bab39c7770
Michele <michele.monti7@studio.unibo.it>
2023-05-15 18:28:19 +0200

    first commit of the branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,10 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    /* module 2.2 - asynchronous programming - Vert.x */
+    implementation("io.vertx:vertx-core:4.4.1")
+    implementation("io.vertx:vertx-web:4.4.1")
+    implementation("io.vertx:vertx-web-client:4.4.1")
 }
 
 tasks.getByName<Test>("test") {
